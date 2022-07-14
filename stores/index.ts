8c1ed87d68cec100924973307518cab6341358f8
ASoldo <soldo.andrej@gmail.com>
2022-07-14 18:31:33 +0200

    Small cleanup

--- stores/index.ts
+++ stores/index.ts
@@ -1,24 +1,14 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useFiltersStore = defineStore('my-store', {
-
-  state: () =>({
-     counter: ref<number>(10)
-  }),
-  persist: true
-
-})
-// export const useFiltersStore = defineStore({
-//   id: 'andrija-store',
-//   state: () => {
-//     return {
-//       filtersLists: ['youtube', 'twitch'] as string[],
-//       counter: 0 as number
-//     }
-//   },
-//   actions: {},
-//   getters: {
-//     filtersList: state => state.filtersLists,
-//   },
-// })
+export const useFiltersStore = defineStore(
+  'my-store', () => {
+    const counter = ref(10)
+    return {
+      counter
+    }
+  },
+  {
+    persist: true
+  }
+)
