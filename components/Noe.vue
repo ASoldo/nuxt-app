a89a6a7f46be2298617cc06bc03b6f05e3417fc6
ASoldo <soldo.andrej@gmail.com>
2022-07-16 04:01:29 +0200

    Update project with server related stuff

---
+++ components/Noe.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot></slot>
+    <slot name="noe-name" />
+    <slot name="noe-age" />
+    <h1>{{ props?.ime }}</h1>
+    <button @click="soldo">Click</button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  ime: {
+    type: String,
+    default: "mnmn",
+  },
+});
+
+const emits = defineEmits(["soldo-emit"]);
+
+const soldo = function () {
+  console.log("Soooldo je okinuo");
+  emits("soldo-emit", {
+    broj: 1000,
+  });
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,72 @@
 <template>
-    <div class="container mx-auto bg-gray-300 p-8">
-        <h1 class="font-bold text-gray-600 text--lg">{{soldo}}</h1>
-        <!-- <h1>{{filtersList[0]}}</h1> -->
-        <h1>{{name}}</h1>
-        <h1>{{triforce}}</h1>
-        <ContentDoc path="/" />
-        <ContentDoc path="/users/user" v-slot="name">
-          <h1 style="color: red;">
-              {{name.doc.name}}
-              {{name.doc.age}}
-          </h1>
-          <slot name="name"></slot>
-            
-           
-        </ContentDoc>
+  <div class="container mx-auto bg-gray-300 p-8">
+    <h1 class="font-bold text-gray-600 text--lg">{{ soldo }}</h1>
+    <!-- <h1>{{filtersList[0]}}</h1> -->
+    <!-- <h1>{{name}}</h1> -->
+    <h1>{{ triforce }}</h1>
+    <ContentDoc path="/" />
+    <ContentDoc path="/users/user" v-slot="name">
+      <h1 style="color: red">
+        {{ name.doc.name }}
+        {{ name.doc.age }}
+      </h1>
+      <slot name="name"></slot>
+    </ContentDoc>
 
-         <ContentQuery path="/" v-slot="{data}">
-            {{data[1].name}}
-            {{data[1].age}}
+    <ContentQuery path="/" v-slot="{ data }">
+      {{ data[1].name }}
+      {{ data[1].age }}
+    </ContentQuery>
 
-        </ContentQuery>
+    <p>{{ user.name }}</p>
+    <p>{{ user.age }}</p>
 
-        <p>{{user.name}}</p>
-        <p>{{user.age}}</p>
-
-        <p>{{customer.user.name}}</p>
-        <p>{{customer.user.age}}</p>
-        <button @click="filtersStore.counter++">Click {{filtersStore.counter}}</button>
-    </div>
+    <p>{{ customer.user.name }}</p>
+    <p>{{ customer.user.age }}</p>
+    <button @click="filtersStore.counter++">
+      Click {{ filtersStore.counter }}
+    </button>
+    <Noe ime="Hej jooooo" @soldo-emit="recieved($event)">
+      <template #noe-name>
+        <h1>Noe name</h1>
+      </template>
+      <template v-slot:noe-age>
+        <h1 class="text-blue-300">32</h1>
+      </template>
+      <h1>theme</h1>
+    </Noe>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { useFiltersStore } from '~/stores/index';
+<script lang="ts" setup>
+import { useFiltersStore } from "~/stores/index";
 
-import {User, Customer} from '~/types/User';
-export default defineComponent({
-    name: "IndexAndrija",
-    setup(){
-        let data = null;
-        let user = ref<User>({
-            name: "Soldo",
-            age: 20
-        });
+import { User, Customer } from "~/types/User";
 
-        let customer = ref<Customer>({
-            user: user.value,
-            created: true
-        });
-        const soldo = "Soldo00" as string;
-        const filtersStore = useFiltersStore();
-        // const filtersList = filtersStore.filtersList;
-        const uhuuu = reactive({
-            name: 'Link' as string
-        });
+let user = ref<User>({
+  name: "Soldo",
+  age: 20,
+});
 
-        let triforce = ref<string>('Linkkkk');
+let customer = ref<Customer>({
+  user: user.value,
+  created: true,
+});
+const soldo = "Soldo00" as string;
+const filtersStore = useFiltersStore();
+// const filtersList = filtersStore.filtersList;
 
-        triforce.value = "sasd";
+// const uhuuu = reactive({
+//     name: 'Link' as string
+// });
 
-        const store = filtersStore.counter;
-        return{
-            soldo,
-            filtersStore,
-            // filtersList,
-            ...toRefs(uhuuu),
-            triforce,
-            data,
-            user,
-            customer,
-            store
-        }
-    }
-})
+let triforce = ref<string>("Linkkkk");
+
+triforce.value = "sasd";
+
+const store = filtersStore.counter;
+
+const recieved = function (param) {
+  console.log("recieved" + param.broj);
+};
 </script>
