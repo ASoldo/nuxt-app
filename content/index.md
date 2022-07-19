7f1e0c5e56455c8e4e7136286767d73c59b1e9c4
ASoldo <soldo.andrej@gmail.com>
2022-07-19 18:08:28 +0200

    Small cleanup: ToDo: Implement Testing framework

--- content/index.md
+++ content/index.md
@@ -3,5 +3,5 @@
 ### Soldo12314
 
 ::soldo
-Ojlaaaa
+#Ojlaaaa
 ::
--- server/api/test/index.test.js
+++
@@ -1,7 +0,0 @@
-import {it, expect} from 'vitest'
-
-const name = 'Soldo'
-
-it("Hello", ()=>{
-    expect(name).toBe('Soldo');
-});
